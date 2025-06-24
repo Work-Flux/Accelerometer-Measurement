a87ba8e45c53926eab855f65bd85260e22923e51
Work-Flux <finnluxton.work@gmail.com>
2025-06-24 14:43:29 +1200

    Seperated out charts and tables into their own files

---
+++ Accelerometer Measurement/ChartView.swift
@@ -0,0 +1,143 @@
+//
+//  ChartView.swift
+//  Accelerometer Measurement
+//
+//  Created by Finn Luxton on 24/06/2025.
+//
+
+import SwiftUI
+import Charts
+
+/*
+ Section for displaying a series of charts to convey details about the acceleration of the device and implications of said acceleration
+ 
+ Contains:
+    Struct grouping charts and displaying them based on selection
+    Structs for the generation of charts according to settings
+ 
+ TODO: Make charts tappable to pop up expanded and scrollable view of the selected one
+ */
+
+// Struct for displaying charts
+struct charts: View {
+    // The stored data
+    var displayedData: [recordedData]
+    
+    // The settings data
+    var currentSettings: [String : Double]
+    
+    // What is the contents of the table
+    @Binding var tableContents: [tableText]
+    
+    // What names do the data entries have and where are they displayed
+    let dataEntries: [String]
+    let dataEntryCount: [Int]
+    @Binding var chartDisplays: [Bool]
+    @Binding var tableDisplays: [Bool]
+    
+    var body: some View {
+        VStack {
+            if chartDisplays[0] {  // Display basic accelerometer data chart
+                baseAccelChart(
+                    displayedData: displayedData,
+                    currentSettings: currentSettings
+                )
+            }
+            
+            if chartDisplays[1] {  // Display ∆M chart
+                magnitudeChart(
+                    displayedData: displayedData,
+                    currentSettings: currentSettings
+                )
+            }
+            
+            if chartDisplays[2] { // Display Equation: Energy chart
+                powerChart(
+                    displayedData: displayedData,
+                    currentSettings: currentSettings
+                )
+            }
+            
+            if chartDisplays[3] { // Display Equation: Current chart
+                
+            }
+            
+            if chartDisplays[4] { // Display Equation: Voltage chart
+                
+            }
+        }
+    }
+}
+
+
+// Displays the baseline xyz accelerometer data
+struct baseAccelChart: View {
+    // The stored data
+    var displayedData: [recordedData]
+    
+    // The settings data
+    var currentSettings: [String : Double]
+    
+    var body: some View {
+        Chart {
+            ForEach(displayedData) { data in
+                if data.index == "X" || data.index == "Y" || data.index == "Z" {
+                    LineMark(
+                        x: .value("Time", data.time),
+                        y: .value("Total Count", data.value)
+                    )
+                    .foregroundStyle(by: .value("Index", data.index))
+                    .interpolationMethod(.cardinal)
+                    .opacity(0.8)
+                }
+            }
+        }
+    }
+}
+
+// Displays the accelerometer magnitude and change in magnitude data
+struct magnitudeChart: View {
+    // The stored data
+    var displayedData: [recordedData]
+    
+    // The settings data
+    var currentSettings: [String : Double]
+    
+    var body: some View {
+        Chart {
+            ForEach(displayedData) { data in
+                if data.index == "M" || data.index == "∆M" {
+                    LineMark(
+                        x: .value("Time", data.time),
+                        y: .value("Total Count", data.value)
+                    )
+                    .foregroundStyle(by: .value("Index", data.index))
+                    .interpolationMethod(.cardinal)
+                }
+            }
+        }
+    }
+}
+
+struct powerChart: View {
+    // The stored data
+    var displayedData: [recordedData]
+    
+    // The settings data
+    var currentSettings: [String : Double]
+    
+    var body: some View {
+        Chart {
+            ForEach(displayedData) { data in
+                if data.index == "P" ||  data.index == "KE" {
+                    LineMark(
+                        x: .value("Time", data.time),
+                        y: .value("Total Count", data.value * (currentSettings["Mass"] ?? 1))
+                    )
+                    .foregroundStyle(by: .value("Index", data.index))
+                    .interpolationMethod(.cardinal)
+                }
+            }
+        }
+    }
+}
---
+++ Accelerometer Measurement/TableView.swift
@@ -0,0 +1,120 @@
+//
+//  TableView.swift
+//  Accelerometer Measurement
+//
+//  Created by Finn Luxton on 24/06/2025.
+//
+
+import SwiftUI
+
+/*
+ Section for creating a table after ending recording of accelerometer data for manual comparison and checking what would be exported as csv via email
+ 
+ Contains:
+    Main struct for displaying tables
+    Data storage struct for contents and order of data sent to tables
+ 
+ TODO: Check modification of power data based on changing settings
+    Might need re-generation from endRecording
+    Possibly seperate out chart generation into new function
+ */
+
+// Struct for displaying tables
+struct tables: View {
+    // The settings data
+    var currentSettings: [String : Double]
+    
+    // What is the contents of the table
+    @Binding var tableContents: [tableText]
+    
+    // What names do the data entries have and where are they displayed
+    let dataEntries: [String]
+    let dataEntryCount: [Int]
+    @Binding var chartDisplays: [Bool]
+    @Binding var tableDisplays: [Bool]
+    
+    var body: some View {
+        // Table display
+        let tableSet = zip(tableDisplays, dataEntryCount)
+        let tableCount: Int = tableSet.map{($0 ? 1 : 0) * $1}.reduce(0, +)
+        let columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: addedValueCount + tableCount)
+        ScrollView{
+            LazyVGrid(columns: columns) {
+                Text("Time")
+                if tableDisplays[0] {
+                    Text("X")
+                    Text("Y")
+                    Text("Z")
+                }
+                
+                if tableDisplays[1] {
+                    Text("M")
+                    Text("∆M")
+                }
+                
+                if tableDisplays[2] {
+                    Text("P")
+                    Text("KE")
+                }
+                
+                if tableDisplays[3] {
+                    
+                }
+                
+                if tableDisplays[4] {
+                    
+                }
+                
+                ForEach(tableContents) { data in
+                    Text(data.T)
+                    if tableDisplays[0] {
+                        Text(data.X)
+                        Text(data.Y)
+                        Text(data.Z)
+                    }
+                    
+                    if tableDisplays[1] {
+                        Text(data.M)
+                        Text(data.dM)
+                    }
+                    
+                    if tableDisplays[2] { //TODO: Cause of inaccuracy from stopRecording log
+                        Text(String(format: "%.5f", (Double(data.P) ?? 0) * (currentSettings["Mass"] ?? 1)))
+                        Text(String(format: "%.5f", (Double(data.KE) ?? 0) * (currentSettings["Mass"] ?? 1)))
+                    }
+                    
+                    if tableDisplays[3] {
+                        
+                    }
+                    
+                    if tableDisplays[4] {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Struct storing data for all potential table columns at a single time
+struct tableText: Identifiable {
+    let T: String // Time
+    let X: String // XYZ acceleration
+    let Y: String
+    let Z: String
+    let M: String // Magnitude of acceleration
+    let dM: String // Change in magnitude between steps
+    let P: String // Specific power from movement
+    let KE: String // Specific kinetic energy from change between steps
+    let id = UUID()
+    
+    init(inputValues: [String]) {self.T = inputValues[0]
+        self.X = inputValues[1]
+        self.Y = inputValues[2]
+        self.Z = inputValues[3]
+        self.M = inputValues[4]
+        self.dM = inputValues[5]
+        self.P = inputValues[6]
+        self.KE = inputValues[7]
+    }
+}
